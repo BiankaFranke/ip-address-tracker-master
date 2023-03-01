34376d220d5c4ada85209dd5eaed39e997795b47
“BiankaFranke” <bianka_franke@hotmail.de>
2023-03-01 11:23:50 +0100

    first try mobile version and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('it works')
+console.log('it works');
+
+// DECLARATIONS
+const btn = document.querySelector('#btnSubmit');
+
+// FUNCTION AND API
+
+
+// EVENTLISTENER
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('yippie');
+})
