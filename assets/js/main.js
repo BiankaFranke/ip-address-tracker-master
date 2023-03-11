08304edba9d6324726b20b5590a058ddffb59bb4
“BiankaFranke” <bianka_franke@hotmail.de>
2023-03-11 15:21:53 +0100

    css and js updated and responsive added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-console.log('it works');
-
 // DECLARATIONS
 const myAPI = config.API;
 const btn = document.querySelector('#btnSubmit');
@@ -8,36 +6,55 @@ const ip = document.querySelector('#ip');
 const ort = document.querySelector('#location');
 const timezone = document.querySelector('#timezone');
 const isp = document.querySelector('#isp');
-let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${myAPI}&ipAddress=${inputIP.value}`
+let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${myAPI}`;
 
-// https://api.ipify.org?format=json
-// https://api.ipify.org
+// INITIAL MAP
+let map = L.map('map', {
+    zoomControl: false,
+    dragging: false,
+}).setView([0, 0], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+map.scrollWheelZoom.disable();
+let marker = L.marker([0, 0]).addTo(map);
+
+// INITIAL API FETCH
+fetch(url)
+.then((response) => response.json())
+.then((data) => {
+    let outputIP = data['ip'];
+    let outputCountry = data['location']['region'];
+    let outputCity = data['location']['city'];
+    let outputTimezone = data['location']['timezone'];
+    let outputIsp = data['isp'];
+
+    ip.innerText = outputIP;
+    ort.innerText = `${outputCountry}, ${outputCity}`;
+    timezone.innerText = `UTC ${outputTimezone}`;
+    isp.innerText = outputIsp;
+    map.setView([data['location']['lat'], data['location']['lng']], 13)
+    marker.setLatLng([data['location']['lat'], data['location']['lng']])
+});
 
 // EVENTLISTENER
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    // findIP();
-    let getIP = inputIP.value;
-
-    if(getIP == '') {
-        getIP = '8.8.8.8';
-    } else {
-        getIP = inputIP.value;
-    };
-
-    fetch(url)
+    fetch(`${url}&ipAddress=${inputIP.value}`)
     .then((response) => response.json())
     .then((data) => {
-        // let outputIP = data['ip'];
+        let outputIP = data['ip'];
         let outputCountry = data['location']['region'];
         let outputCity = data['location']['city'];
         let outputTimezone = data['location']['timezone'];
         let outputIsp = data['isp'];
 
-        ip.innerText = getIP;
+        ip.innerText = outputIP;
         ort.innerText = `${outputCountry}, ${outputCity}`;
         timezone.innerText = `UTC ${outputTimezone}`;
         isp.innerText = outputIsp;
+        map.setView([data['location']['lat'], data['location']['lng']], 13)
+        marker.setLatLng([data['location']['lat'], data['location']['lng']])
     });
-    
 })
